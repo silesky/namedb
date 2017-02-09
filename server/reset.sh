0a9ac7e619209b3e57b1b2321d2f5fc33420a4d1
silesky <seth.silesky@gmail.com>
2017-02-09 15:48:31 -0600

    update reset

--- server/reset.sh
+++ server/reset.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
 rm -rf Migrations/*
-dotnet ef migrations add "initialMigration"
-dotnet run
+psql -t myndb -c "select 'drop table \"' || tablename || '\" cascade;' from pg_tables where schemaname = 'public'" | psql myndb
+dotnet ef migrations add `date +%Y-%m-%d:%H:%M:%S` && echo "migration complete!"
+dotnet ef database update && echo "database updated!"
+dotnet build
